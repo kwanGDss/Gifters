d949437b8fda8dd237f818d009ec60343edacde6
Jung Kwan Young <66257657+kwanGDss@users.noreply.github.com>
2022-11-24 02:41:15 +0900

    Implement fire(hitscan)

--- Source/Gifters/Private/MyGiftersCharacter.cpp
+++ Source/Gifters/Private/MyGiftersCharacter.cpp
@@ -2,4 +2,106 @@
 
 
 #include "MyGiftersCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+AMyGiftersCharacter::AMyGiftersCharacter()
+{
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -97.0f), FRotator(0.0f, -90.0f, 0.0f));
+
+	GetFollowCamera()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 200.0f), FRotator(-25.0f, 0.0f, 0.0f));
+	GetCameraBoom()->TargetArmLength = 300.0f;
+
+	GetCharacterMovement()->JumpZVelocity = 300.0f;
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_Drongo(TEXT("/Game/ParagonDrongo/Characters/Heroes/Drongo/Meshes/Drongo_GDC"));
+	if (SK_Drongo.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SK_Drongo.Object);
+	}
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> ABP_Drongo(TEXT("/Game/ParagonDrongo/Characters/Heroes/Drongo/Drongo_AnimBlueprint"));
+	if (ABP_Drongo.Succeeded())
+	{
+		GetMesh()->SetAnimInstanceClass(ABP_Drongo.Class);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UAnimMontage> AM_Drongo(TEXT("/Game/ParagonDrongo/Characters/Heroes/Drongo/Animations/Primary_Fire_Montage"));
+	if (AM_Drongo.Succeeded())
+	{
+		AttackMontage = AM_Drongo.Object;
+	}
+
+	bIsAttacking = false;
+	bSaveAttack = false;
+	AttackCount = 0;
+}
+
+void AMyGiftersCharacter::Attack()
+{
+	if (bIsAttacking)
+	{
+		bSaveAttack = true;
+	}
+	else
+	{
+		bIsAttacking = true;
+
+		PlayAttackMontage();
+	}
+}
+
+void AMyGiftersCharacter::ComboAttackSave()
+{
+	if (bSaveAttack)
+	{
+		bSaveAttack = false;
+
+		PlayAttackMontage();
+	}
+}
+
+void AMyGiftersCharacter::ResetCombo()
+{
+	AttackCount = 0;
+	bSaveAttack = false;
+	bIsAttacking = false;
+}
+
+void AMyGiftersCharacter::Fire()
+{
+	FHitResult HitResult;
+	FVector StartedFire = UKismetMathLibrary::TransformLocation(GetActorTransform(), FVector(100.0f, 20.0f, 57.0f));
+	FVector EndedFire = GetActorForwardVector() * 15000.0f + StartedFire;
+	TArray<AActor*> IgnoreActors;
+
+	UKismetSystemLibrary::LineTraceSingleByProfile(GetWorld(), StartedFire, EndedFire, "Fire", false, IgnoreActors, EDrawDebugTrace::ForOneFrame, HitResult, true);
+	UKismetSystemLibrary::DrawDebugBox(GetWorld(), StartedFire, FVector::OneVector, FLinearColor::Red, FRotator::ZeroRotator, 10.0f);
+	UKismetSystemLibrary::PrintString(GetWorld(), TEXT("Fire"));
+}
+
+void AMyGiftersCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &AMyGiftersCharacter::Attack);
+}
+
+void AMyGiftersCharacter::PlayAttackMontage()
+{
+	switch (AttackCount)
+	{
+	case 0:
+		AttackCount = 1;
+		PlayAnimMontage(AttackMontage);
+		break;
+	case 1:
+		AttackCount = 0;
+		PlayAnimMontage(AttackMontage);
+		break;
+	default:
+		break;
+	}
+}
--- Source/Gifters/Public/MyGiftersCharacter.h
+++ Source/Gifters/Public/MyGiftersCharacter.h
@@ -13,5 +13,38 @@ UCLASS()
 class GIFTERS_API AMyGiftersCharacter : public AGiftersCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	AMyGiftersCharacter();
+
+	UFUNCTION(BlueprintCallable)
+	void Attack();
+
+	UFUNCTION(BlueprintCallable)
+	void ComboAttackSave();
+
+	UFUNCTION(BlueprintCallable)
+	void ResetCombo();
+
+	UFUNCTION(BlueprintCallable)
+	void Fire();
+
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void PlayAttackMontage();
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = true))
+	bool bIsAttacking;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = true))
+	bool bSaveAttack;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = true))
+	int AttackCount;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attack", meta = (AllowPrivateAccess = true))
+	UAnimMontage* AttackMontage;
 };
